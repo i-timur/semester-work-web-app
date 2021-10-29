79932538f95e814f519865d0180fb8318c8f1b33
i-timur <i.timur@mail.ru>
2021-10-29 22:38:19 +0300

    css layout is finished

--- src/main/java/ru/kpfu/ibragimov/filters/SecurityFilter.java
+++ src/main/java/ru/kpfu/ibragimov/filters/SecurityFilter.java
@@ -1,9 +1,5 @@
 package ru.kpfu.ibragimov.filters;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpFilter;
-
-@WebServlet("/*")
-public class SecurityFilter extends HttpFilter {
+public class SecurityFilter {
 
 }
