4f7d71de8b215da11f14ca4d5a0fd366557fb667
i-timur <i.timur@mail.ru>
2021-11-01 14:00:53 +0300

    logout is finished

--- src/main/java/ru/kpfu/ibragimov/controller/LogoutServlet.java
+++ src/main/java/ru/kpfu/ibragimov/controller/LogoutServlet.java
@@ -1,4 +1,19 @@
 package ru.kpfu.ibragimov.controller;
 
-public class LogoutServlet{
+import ru.kpfu.ibragimov.service.SecurityService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    SecurityService.logout(req);
+    resp.sendRedirect("/");
+  }
 }
